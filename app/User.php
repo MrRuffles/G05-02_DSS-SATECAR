0596bce21eccacbfd67ab0a78adc92fa5fedeb09
jsm134 <jsm134@alu.ua.es>
2020-02-23 18:38:04 +0100

    Creado modelo de usuarios

--- app/User.php
+++ app/User.php
@@ -9,31 +9,33 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'dni','name', 'surnames' , 'adress', 'phone' ,'typeUser' , 'email',
     ];
-
+    
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
+    /*protected $hidden = [
         'password', 'remember_token',
-    ];
+    ];*/
 
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = [
+   /* protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
+    ];*/
+    public function cars(){
+        return $this->belongsToMany('App\Car');
+    }
 }

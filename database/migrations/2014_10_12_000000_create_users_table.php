bd91cf1239d90785918b41fa8a966d2b7724caf3
jsm134 <jsm134@alu.ua.es>
2020-02-23 18:03:00 +0100

    Creada migración de la tabla user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('dni');
             $table->string('name');
+            $table->string('surnames');
+            $table->string('adress');
+            $table->string('phone');
+            $table->string('typeUser'); 
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

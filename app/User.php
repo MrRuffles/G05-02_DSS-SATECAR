fcf09ec6a2160d8ae223bd7a2aa8ed392ebff944
FranciscoJavierRicoPerez <fjrp8@alu.ua.es>
2020-02-24 17:38:23 +0100

    Creado modelo de datos

--- app/User.php
+++ app/User.php
@@ -9,6 +9,11 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+
+    public function cars(){
+        return $this->belongsToMany('App\Car');
+    }
+
     /**
      * The attributes that are mass assignable.
      *
@@ -35,7 +40,4 @@ class User extends Authenticatable
    /* protected $casts = [
         'email_verified_at' => 'datetime',
     ];*/
-    public function cars(){
-        return $this->belongsToMany('App\Car');
-    }
 }

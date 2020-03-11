078a9477f3b933e965cbbbe5f6ff9c3a4c5b9ab4
Carlos Torralba Ruiz <carlostorralba12@gmail.com>
2020-03-11 17:51:59 +0100

    Añado Ruta Cars

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /*
@@ -10,7 +11,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use App\Car;
 Route::get('/', function () {
     return view('paginaprincipal');
+});
+
+Route::get('cars', function(){
+    return view('cars'/*, ['cars' => Car::all()]*/)->with('cars', Car::all()) ;
 });

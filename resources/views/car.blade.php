57c2d4c6fed616f2fc60e47dd772a751bc8ebb67
Carlos Torralba Ruiz <carlostorralba12@gmail.com>
2020-03-11 18:17:09 +0100

    Añado Vista de un solo Car

---
+++ resources/views/car.blade.php
@@ -0,0 +1,33 @@
+@include('barradenavegacion')
+<!doctype html>
+
+<html>
+<h1> Car </h1>
+<table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Enrollment</th>
+      <th scope="col">Years</th>
+      <th scope="col">Km</th>
+      <th scope="col">TradeMark</th>
+      <th scope="col">Color</th>
+      <th scope="col">FuelConsumtion</th>
+      <th scope="col">Brand</th>
+    </tr>
+  </thead>
+  <tbody> 
+    <tr>
+        <th scope="row">Car {{$car->id}}</th> 
+        <td>{{$car->enrollment}}</td>
+        <td>{{$car->years}}</td>
+        <td>{{$car->km}}</td>
+        <td>{{$car->tradeMark}}</td>
+        <td>{{$car->color}}</td>
+        <td>{{$car->fuelConsumption}}</td>
+        <td>{{$car->brand_id}}</td>
+    </tr>
+    
+</tbody>
+</table>
+</html>

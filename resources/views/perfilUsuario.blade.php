9c339c65dbbc145c90263695911a7391a34ae814
FranciscoJavierRicoPerez <fjrp8@alu.ua.es>
2020-03-17 12:10:04 +0100

    Datos del usuario en el perfil terminados

--- resources/views/perfilUsuario.blade.php
+++ resources/views/perfilUsuario.blade.php
@@ -13,7 +13,17 @@
 <h3>Perfil de Usuario</h3>
 <div  style="padding: 10px; border-style: dashed; border-width: 1px; margin-bottom: 11px">
     <div class="row" style="justify-content: space-between; margin:0px">
-        
+        <label>DNI: {{ $usuario->dni }}</label>
+        <div>
+        <button href="#" type="button" class="btn btn-primary">Editar Perfil</button>
+        <button href="#" type="button" class="btn btn-danger">Eliminar Perfil</button>
+        </div>
     </div>
+    <p>Nombre: {{ $usuario->name }}</p>
+    <p>Apellidos: {{ $usuario->surnames }}</p>
+    <p>Telefono: {{ $usuario->phone }}</p>
+    <p>Dirección: {{ $usuario->adress }}</p>
+    <p>Correo Electronico: {{ $usuario->email }}</p>
+    <h3>Tipo de Usuario: {{ $usuario->typeUser }}</h3>
 </div>
 @endsection
